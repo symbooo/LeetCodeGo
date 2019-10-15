b2cbcbd20b58139c4972bf5223ea14dc09d2f174
symb <echolan@126.com>
2019-10-15 19:21:48 +0800

    LeetCode No.20 valid parentheses

---
+++ No20_ValidParentheses.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isValid(s string) bool {
+
+	if s == "" {
+		return true
+	}
+
+	d := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	stack := []rune{}
+	for _, c := range s {
+		if len(stack) == 0 {
+			if _, ok := d[c]; ok {
+				return false
+			} else {
+				stack = append(stack, c)
+				continue
+			}
+		}
+		if k, ok := d[c]; ok {
+			if stack[len(stack)-1] == k {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		} else {
+			stack = append(stack, c)
+		}
+	}
+	if len(stack) == 0 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	b := isValid("[][][[[]]")
+	fmt.Println("isValid: ", b)
+}
